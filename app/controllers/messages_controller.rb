07404a0a7b76f877b47e56c3bab1021f02ac97a3
annamori <annyon_25@yahoo.co.jp>
2017-01-28 13:27:56 +0000

    challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,24 +1,9 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update, :destroy, :age]
+  before_action :set_message, only: [:edit, :update, :destroy]
+
   def index
     @message = Message.new
     @messages = Message.all
-  end
-  
-  def edit
-  end
-  
-  def update
-    if @message.update(message_params)
-      redirect_to root_path , notice: 'メッセージを編集しました'
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @message.destroy
-    redirect_to root_path, notice: 'メッセージを削除しました'
   end
   
   def create
@@ -32,8 +17,29 @@ class MessagesController < ApplicationController
    end
   end
 
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+
   private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
   def message_params
-   params.require(:message).permit(:name, :body)
+   params.require(:message).permit(:name, :body, :age)
   end
 end
